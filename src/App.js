8886dd7d3eba178b2c5f107ff6a12811956442b6
loafcheck <rachelskim.contact@gmail.com>
2024-03-28 12:50:29 -0700

    interior ship

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import interior from './interior.png';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function App() {
+  return (
+    <div className="App">
+    <>
+      <Navbar className='Nav'>
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </>
+
+    <>
+      <div className='main-bg' style={{backgroundImage: `url(${interior})`}}></div>
+    </>
+
+    <>
+    <Container>
+      <Row md={4}>
+        <Col>1 of 3</Col>
+        <Col xs={6}>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+    </>
+    </div>
+  );
+}
+
+export default App;
